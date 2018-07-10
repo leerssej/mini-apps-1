5b1939de80c85d9745a9e9a1efca355dd11872e0
leerssej <jens.leerssen@gmail.com>
2018-07-10 14:52:12 -0700

    gets a hello world server running installs jquery and gets simple button handling set up passes in a value out to the console.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,20 @@
 
+$(document).ready(() => {
+  console.log("doc loaded");
+  $('.submit').click(function() {
+    console.log('submit clicked');
+    let inputJson = $('#json-input').val();
+    sendDataToServer(inputJson);
+  });
+});
 
+// $("#target").click(function () {
+//   alert("Handler for .click() called.");
+// });
+
+// // A $( document ).ready() block.
+// $(document).ready(function () {
+//   console.log("ready!");
+// });
+
+console.log("Script Was Run outside of docReady")
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,23 +4,26 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// add logging middleware
 // parse body
-// GET request
 // to pull all data out of the server
-// POST request
 // to load the data into the server
 // set server to listen
 
 
 // // serve static assets
-// app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
-// app.use(function (request, res, next) {
-//   console.log(request.method, request.path);
-//   next
-// })
+// add logging middleware
+app.use(function (request, response, next) {
+  console.log(request.method, request.path);//, response);
+  next();
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// GET request
+app.get('/data', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// POST request
+app.post('/data', (req, res) => res.send('Hello Mars!'));
+
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
