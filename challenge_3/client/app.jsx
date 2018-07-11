767b06dc6d5a05f0811e24ef2294b6ea269d1a15
leerssej <jens.leerssen@gmail.com>
2018-07-11 15:25:58 -0700

    serves page to localhost:3000

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// var App = () => <div>Hello World</div>;
+// ReactDOM.render(<App />, document.getElementById('app'));
+const App = () => (
+  <div>Some cliche salutation</div>
+);
 
+ReactDOM.render(<App />, document.getElementById("app"));
+
+export default App;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const path = require('path');
+const express = require('express')
+const app = express()
 
+// server static assets
+app.use(express.static(path.join(__dirname, './public')));
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
