e901c2a200cdc5b97aef4ced425e53861a6f5651
leerssej <jens.leerssen@gmail.com>
2018-07-11 15:32:23 -0700

    sets up server with POST response to /account endpoint

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // server static assets
 app.use(express.static(path.join(__dirname, './public')));
 
+app.post('/account', (req, res) => res.send('Hello, Accounting here!'));
 // app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('VirtualStoreServer listening on port 3000!'));
 
