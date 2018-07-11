04108733fecd65308748c75b7399b8493165db3c
leerssej <jens.leerssen@gmail.com>
2018-07-10 20:10:40 -0700

    importing transform.traverse and sending response back to page

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,9 @@
 // install dependencies
-const http = require('http');
+// const http = require('http');
 const path = require('path');
 const express = require('express');
 const app = express();
+const transform = require(path.join(__dirname, './traverse.js'));
 
 // parse body
 // to pull all data out of the server
@@ -27,13 +28,11 @@ app.use(bodyparser.json());
 // POST request
 // app.post('/data', (req, res) => res.send('Hello Mars!'+ req));
 app.post('/data', (req, res) => {
-  res.send("Thanks for the Post!")
-  // console.log(JSON.stringify(req.body));
-  // let sentData = req.body;
-  // console.log(sentData);
+  // console.log(JSON.stringify(transform.traverse(req.body)));
+  res.send(transform.traverse(req.body));
 });
 
 // GET request
 app.get('/data', (req, res) => res.send('Hello World!'));
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'));
+app.listen(3001, () => console.log('Example app listening on port 3001!', transform));
