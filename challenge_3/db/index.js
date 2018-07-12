8836595aa1e6a00a3407ffd790f9e883e17365aa
leerssej <jens.leerssen@gmail.com>
2018-07-11 23:22:01 -0700

    implements working addRecord function

--- challenge_3/db/index.js
+++ challenge_3/db/index.js
@@ -9,12 +9,40 @@ let connection = mysql.createConnection({
   plugin: 'mysql_native_password'
 });
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT * FROM orders', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
+// test to see that I can get data back from the databases
+function getAllOrderRecords() {
+  connection.query('SELECT * FROM orders', (e, s, fields) => {
+    if (e) {
+      throw e;
+    } else {
+      console.log('The solution is: ', s);
+    }
+  });
+}
+// getAllOrderRecords();
+
+// let orderColumns = [visitTimeStamp, username, email, password, addrline1, addrline2, city, state, shipzip, phone, ccnum, expdate, cvv, billzip];
+
+const addRecord = function (visitTimeStamp, username, email, callback) {
+  let queryScript = `INSERT INTO orders (visitTimeStamp, username, email) VALUES (?, ?, ?);`;
+  connection.query(queryScript, [visitTimeStamp, username, email], callback);
+}
+
+// // module.exports = {addRecord}
+
+let exampleValues = ["Wed Jul 11 2018", "jens_leerssen", 'notarealaccount@gmail'];
+
+// let exampleValues = ["Wed", "jens_leerssen", "jens.leerssen@email.com", "password1", "14 Holten Avenue", "Apt 16B", "Newport", "RI", "02840", "415-555-1212", "1234567890123456", '2018-10-06', 201, "01394-4560"];
+
+addRecord("Wed Jul 11 2018", "jens_leerssen", 'notarealaccount@gmail', function(e,s){
+  if (e) {
+    console.log("error: " + e, null);
+  } else {
+    getAllOrderRecords((e,s) => console.log(e,s));
+  }
 });
 
-connection.end();
 
+// connection.end();
