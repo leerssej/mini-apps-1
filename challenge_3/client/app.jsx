cc5a4c6d7cec955780e96e44089c2deaa35d327a
leerssej <jens.leerssen@gmail.com>
2018-07-11 19:03:54 -0700

    cycles through checkout, accountEntry and ConfirmClick states and back again

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,11 +1,64 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      isCheckingOut: false,
+      isAccountInfoSubmitted: false,
+      date: new Date(),
+      name: '',
+      email: '',
+      password: '',
+    }
+    // click handling
+    this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
+  } // closes constructor
+  
+  handleCheckoutClick() {
+    this.setState({ isCheckingOut: true })
+    console.log(this.state);
+  }
 
-// var App = () => <div>Hello World</div>;
-// ReactDOM.render(<App />, document.getElementById('app'));
-const App = () => (
-  <div>Some cliche salutation</div>
-);
+  handleAccountClick() {
+    this.setState({ isAccountInfoSubmitted: true })
+    console.log(this.state);
+  }
 
+  handleConfirmClick() {
+    this.setState({ isCheckingOut: false })
+    this.setState({ isAccountInfoSubmitted: false })
+    console.log(this.state);
+  }
+
+  render() {
+    if (!this.state.isCheckingOut) {
+      return (
+        <div>
+          <h2>React Store</h2>
+          <span> 
+            <input type="submit" className="checkout" value="Checkout" onClick={this.handleCheckoutClick}/>
+          </span>
+        </div>
+      )
+    } else if (!this.state.isAccountInfoSubmitted){
+      return (
+        <div>
+          <h3>Checking Out</h3>
+          <h4>Account Info</h4>
+          <input type="submit" className="AccountSubmit" value="Next" onClick={this.handleAccountClick.bind(this)}/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h3>Please confirm your order information below.</h3>
+          <h4></h4>
+          <input type="submit" className="OrderSubmit" value="Confirm Order" onClick={this.handleConfirmClick.bind(this)} />
+        </div>
+      )
+    }
+  }
+}
 console.log("app was run")
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,12 +1,12 @@
-const path = require('path');
+// const path = require('path');
 const express = require('express');
 const app = express();
 
 // server static assets
-app.use(express.static(path.join(__dirname, './public')));
+// app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static('public'));
 
 app.post('/account', (req, res) => res.send('Hello, Accounting here!'));
 // app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('VirtualStoreServer listening on port 3000!'));
-
