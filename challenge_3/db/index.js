1e537dadb90f86170a811ad3cfe40cc9a7d10190
leerssej <jens.leerssen@gmail.com>
2018-07-11 23:44:45 -0700

    implements complete working addRecord function

--- challenge_3/db/index.js
+++ challenge_3/db/index.js
@@ -25,18 +25,18 @@ function getAllOrderRecords() {
 
 // let orderColumns = [visitTimeStamp, username, email, password, addrline1, addrline2, city, state, shipzip, phone, ccnum, expdate, cvv, billzip];
 
-const addRecord = function (visitTimeStamp, username, email, callback) {
-  let queryScript = `INSERT INTO orders (visitTimeStamp, username, email) VALUES (?, ?, ?);`;
-  connection.query(queryScript, [visitTimeStamp, username, email], callback);
+const addRecord = function (visitTimeStamp, username, email, password, addrline1, addrline2, city, state, shipzip, phone, ccnum, expdate, cvv, billzip, callback) {
+  let queryScript = `INSERT INTO orders (visitTimeStamp, username, email, password, addrline1, addrline2, city, state, shipzip, phone, ccnum, expdate, cvv, billzip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+  connection.query(queryScript, [visitTimeStamp, username, email, password, addrline1, addrline2, city, state, shipzip, phone, ccnum, expdate, cvv, billzip], callback);
 }
 
 // // module.exports = {addRecord}
 
-let exampleValues = ["Wed Jul 11 2018", "jens_leerssen", 'notarealaccount@gmail'];
+// let exampleValues = ["Wed Jul 11 2018", "jens_leerssen", 'notarealaccount@gmail'];
 
-// let exampleValues = ["Wed", "jens_leerssen", "jens.leerssen@email.com", "password1", "14 Holten Avenue", "Apt 16B", "Newport", "RI", "02840", "415-555-1212", "1234567890123456", '2018-10-06', 201, "01394-4560"];
+let exampleValues = ["Wed", "jens_leerssen", "jens.leerssen@email.com", "password1", "14 Holten Avenue", "Apt 16B", "Newport", "RI", "02840", "415-555-1212", "1234567890123456", '2018-10-06', 201, "01394-4560"];
 
-addRecord("Wed Jul 11 2018", "jens_leerssen", 'notarealaccount@gmail', function(e,s){
+addRecord(...exampleValues, function(e,s){
   if (e) {
     console.log("error: " + e, null);
   } else {
